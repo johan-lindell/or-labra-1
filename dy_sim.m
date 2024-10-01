b310dc40bf0ff84dd1e78596adb29d59302216c9
lindejoh <johan.lindell@aalto.fi>
2024-10-01 19:07:39 +0300

    simulation files added

--- dy_sim.m
+++ dy_sim.m
@@ -1,22 +1,17 @@
-function Xdot = dy(X, cl)
+function Xdot = dy_sim(X, cl, m, cd0, k, s, g, rho)
 
 % Test push
 % MS-E2132 - Laboratory Assignments in Operations Research II, assignment 1
 % State equations of the glider
-
-% parameters
-m = 100;	    % mass
-cd0 = 0.034;    % zero drag coefficient
-k = 0.07;	    % induced drag coefficient
-s = 14;		    % reference area
-g = 9.809;	    % gravitational acceleration
-rho = 1.13;	    % air density 
 
 % state variables
 x = X(1);       % x-coordinate
 h = X(2);       % altitude
 v = X(3);       % velocity
 gamma = X(4);   % flight path angle
+
+%stay level
+cl = g*cos(gamma)/(v*s*0.5*rho*v/m);
 
 % state equations
 xdot = v * cos(gamma);      % Horizontal velocity
---
+++ simulate_flight.m
@@ -0,0 +1,48 @@
+function simulate_flight(X0, tspan, m, cd0, k, s, g, rho, cl)
+    % This function simulates flight dynamics and plots the results.
+    % Parameters:
+    % X0 - Initial state vector [x; h; v; gamma]
+    % tspan - Time span for simulation [start, end]
+    % m - Mass of the aircraft (kg)
+    % cd0 - Zero drag coefficient
+    % k - Induced drag coefficient
+    % s - Reference area (m^2)
+    % g - Gravitational acceleration (m/s^2)
+    % rho - Air density (kg/m^3)
+    % cl - Lift coefficient
+
+    % Solve the state equations using ode45
+    [t, X] = ode45(@(t, X) dy_sim(X, cl, m, cd0, k, s, g, rho), tspan, X0);
+
+    % Plot the results
+    figure;
+    subplot(4, 1, 1);
+    plot(t, X(:, 1));
+    title('x-coordinate');
+    xlabel('Time (s)');
+    ylabel('x (m)');
+
+    subplot(4, 1, 2);
+    plot(t, X(:, 2));
+    title('Altitude');
+    xlabel('Time (s)');
+    ylabel('h (m)');
+
+    subplot(4, 1, 3);
+    plot(t, X(:, 3));
+    title('Velocity');
+    xlabel('Time (s)');
+    ylabel('v (m/s)');
+
+    subplot(4, 1, 4);
+    plot(t, X(:, 4));
+    title('Flight Path Angle');
+    xlabel('Time (s)');
+    ylabel('gamma (rad)');
+
+    % Add parameters to the figure title
+    params_str = sprintf(['m = %d kg, S = %.2f m^2, \\rho = %.2f kg/m^3, g = %.3f m/s^2\n', ...
+        'x_0 = %.2f m, h_0 = %.2f m, v_0 = %.2f m/s, \\gamma_0 = %.2f rad'], ...
+        m, s, rho, g, X0(1), X0(2), X0(3), X0(4));
+    sgtitle(params_str);
+end
